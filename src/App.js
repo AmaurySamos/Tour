440da41851da0082b522aa935372002c6f687e7d
Amaury Samos <amauryrsamos17@gmail.com>
2021-03-10 20:43:39 -0600

    First commit tour project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'C:/Users/Amy/Desktop/Tour/tour-project/src/css/plugins.css';
+import 'C:/Users/Amy/Desktop/Tour/tour-project/src/css/style.css';
+import 'C:/Users/Amy/Desktop/Tour/tour-project/src/css/templete.css';
+import 'C:/Users/Amy/Desktop/Tour/tour-project/src/css/skin-1.css';
+import 'C:/Users/Amy/Desktop/Tour/tour-project/node_modules/slick-carousel/slick/slick.css';
+import 'C:/Users/Amy/Desktop/Tour/tour-project/node_modules/slick-carousel/slick/slick-theme.css';
 
+import Markup from 'C:/Users/Amy/Desktop/Tour/tour-project/src/js/markup/Markup';
+
+
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Markup />
     </div>
   );
 }
